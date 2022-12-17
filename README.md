091489f2f4b00ec9b7eadc5a341fa2994b2faa1e
michelleezhang <mhzhang1442@gmail.com>
2022-12-16 19:38:32 -0600

    frontend initialization

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # water-safety-visualization
+This is a little project for me to learn new programming skills :)
+
+Currently figuring out:
+- Flask
+    - requests
+- Connecting frontend and backend
+- Error handling for user inputs
+
+Future TODOs:
+- (possibly) Transition to Java backend for efficiency
+- Nicer frontend
+- Concentration of each contaminant
+- Pop up information on each contaminant
+- Options for outside the US
+- Interactive map?
+
+Completed: 
+- using pandas
+    - dealing with large databases
+    - accessing and searching for data
--- main.py
+++ main.py
@@ -3,23 +3,30 @@ import pandas as pd
 all_data = pd.read_csv('data/UCMR4_All.txt', sep='	', header=None, low_memory=False)
 zipcode_data = pd.read_csv('data/UCMR4_ZipCodes.txt', sep='	', header=None)
 
-input_zipcode = '60201'
-
-result_set = set() # empty set
-
-# convert zipcode to PWSID
-for i in range(len(zipcode_data)):
-    if zipcode_data.loc[i,1] == input_zipcode:
-        input_PWSID = zipcode_data.loc[i,0]
-
-# find desired row in data
-for j in range(len(all_data)):
-    if input_PWSID == all_data.loc[j,0]: 
-        # for some reason, PWSID's type in the data table is "pandas series", so we convert to string here
-        selected_row = all_data.loc[j,:]
-        result_set.add(selected_row[13])
-        # OFTEN HAS MULTIPLE ROWS
-print(result_set)
 
 
+def find_contaminents(input_zipcode):
+    result_set = set() # empty set
 
+    # convert zipcode to PWSID
+    for i in range(len(zipcode_data)):
+        if zipcode_data.loc[i,1] == input_zipcode:
+            input_PWSID = zipcode_data.loc[i,0]
+
+    # find desired row in data
+    for j in range(len(all_data)):
+        if input_PWSID == all_data.loc[j,0]: 
+            # for some reason, PWSID's type in the data table is "pandas series", so we convert to string here
+            selected_row = all_data.loc[j,:]
+            result_set.add(selected_row[13])
+            # OFTEN HAS MULTIPLE ROWS
+    print(result_set)
+
+
+from flask import Flask 
+from flask import request 
+ 
+if request.method == 'POST': 
+	my_zipcode = request.form['zipcode']
+
+find_contaminents(my_zipcode) 
